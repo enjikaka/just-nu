84ea703dcc443b272a6467378511e71061288e2a
Jeremy Karlsson <karlsson@jeremy.se>
2016-08-15 17:36:48 +0200

    Improve checks to only include JUST NU in upper cast with :

---
+++ just-nu.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const http = require("http");
+
+const options = {
+  host: 'www.aftonbladet.se',
+  port: 80
+};
+
+let justNu = 0;
+
+function getJustNuCount (string) {
+  return (string.match(/JUST NU:/g) || []).length;
+}
+
+function reportAndClose() {
+  console.log('Antal "JUST NU" just nu:', justNu);
+  process.exit(0);
+}
+
+const req = http.request(options, (res) => {
+  res.setEncoding('utf8');
+
+  res.on('data', (chunk) => {
+    justNu += getJustNuCount(chunk);
+  });
+
+  res.on('end', reportAndClose);
+});
+
+req.end();
