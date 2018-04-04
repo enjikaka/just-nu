83a45e04f85d6650e32e796353a12c98ba803a81
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2018-04-04 16:14:16 +0200

    Fix package

--- index.js
+++ index.js
@@ -1,104 +1,46 @@
-#! /usr/bin/env node
+const fetch = require('node-fetch');
 
-'use strict';
-
-const http = require('http');
-
-/**
- * Finds multiple "subtitles" in a paragraph and returns them in an array.
- *
- * @param {String} text - String to search in
- * @return {String[]} - Array of topics found.
- */
-function getParagraphParts (text) {
-  const parts = text.split('<span');
-  const subtitles = [];
-
-  function containsArrow (str) {
-    return str.indexOf('abIconArrow') !== -1;
-  }
-
-  function containsCheckmark (str) {
-    return str.indexOf('abSymbBo') !== -1;
-  }
-
-  for (let part of parts) {
-    if (containsArrow(part) || containsCheckmark(part)) {
-      let symbol = part.split('</span>')[0]
-        , subtitle = part.split('</span>')[1];
-
-      symbol = containsArrow(symbol) ? '➞' : '√';
-
-      if (subtitle.indexOf('</p>') !== -1) {
-        subtitle = subtitle.split('</p>')[0];
-      }
-
-      subtitle = subtitle.trim();
-
-      subtitles.push({
-        symbol,
-        subtitle
-      });
-    }
-  }
-
-  return subtitles;
-}
-
-/**
- * Finds multiple "JUST NU" in a string and returns them in an array.
- *
- * @param {String} text - String to search in
- * @return {String[]} - Array of topics found.
- */
-function getTopics (text) {
-  const justNus = text.split(/JUST NU:/g);
-  let arr = [];
-
-  for (let justNu of justNus) {
-    if (justNu.indexOf('</span>') !== -1 && justNu.indexOf('</h2>') !== -1) {
-      let header = justNu.split('</span>')[1].split('</h2>')[0].replace(/\s+/, '')
-        , paragraphLine
-        , subtitles
-        ;
-
-      if (header && header.indexOf('<') === -1) {
-        paragraphLine = justNu.split(header)[1].split('</p>')[0];
-
-        if (paragraphLine) {
-          subtitles = getParagraphParts(paragraphLine);
-        }
-
-        header = header.replace(/\s+/g, ' ').trim();
-
-        arr.push({
-          header,
-          subtitles
-        });
-      }
-    }
-  }
-
-  return arr;
-}
+const smoosh = (a, b) => a.concat(b);
 
 module.exports = function (callback) {
-  let justNuCount = 0
-    , justNuTopics = []
-    ;
+  // @ts-ignore
+  return fetch('http://aftonbladet.se')
+    .then(r => r.text())
+    .then(text => {
+      const json = JSON.parse(text.split('<script>window.FLUX_STATE = ')[1].split('</script>')[0]);
+      const collectionKey = Object.keys(json.collections)[0];
+      // @ts-ignore
+      const items = Object.values(json.collections[collectionKey].contents.items);
 
-  http.request({ host: 'www.aftonbladet.se' }, res => {
-    res.setEncoding('utf8');
+      const filteredItems = items
+        .filter(item => JSON.stringify(item).indexOf('JUST NU') !== -1)
+        .map(item => {
+          if (item.type === 'widget') {
+            return item.items;
+          }
 
-    res.on('data', chunk => {
-      justNuTopics = justNuTopics.concat(getTopics(chunk));
+          return item;
+        })
+        .reduce(smoosh)
+        .map(item => {
+          item.items = item.items.filter(item => JSON.stringify(item).indexOf('JUST NU') !== -1);
+          return item;
+        });
+
+      const topics = filteredItems.map(item => {
+        return item.items.map(deepItem => {
+          if (deepItem.title && deepItem.title.value.indexOf('JUST NU') !== -1) {
+            return deepItem.title.value;
+          }
+
+          if (deepItem.text && deepItem.text.value.indexOf('JUST NU') !== -1) {
+            return deepItem.text.value;
+          }
+        });
+      }).reduce(smoosh);
+
+      const count = topics.length;
+
+      return { count, topics };
     });
-
-    res.on('end', () => {
-      const count = justNuTopics.length;
-      const topics = justNuTopics;
-      
-      callback({ count, topics });
-    });
-  }).end();
 };
